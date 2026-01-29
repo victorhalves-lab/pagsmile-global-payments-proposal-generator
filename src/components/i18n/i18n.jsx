0945e7263c6883a4e59d595b87198ccd6382a920
Victor Hugo Alves <victorhalves@gmail.com>
2026-01-29 18:59:17 +0000

    File changes

--- src/components/i18n/i18n.jsx
+++ src/components/i18n/i18n.jsx
@@ -1,7 +1,8 @@
+
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import en from './locales/en.json';
-import pt from './locales/pt.json';
+import en from './locales/en';
+import pt from './locales/pt';
 
 const savedLanguage = localStorage.getItem('language') || 'pt';
 
